7cd3f7a88eaded2bd03e3ed54637f5ecd457b066
Charles Loomis <cal@sixsq.com>
2013-02-05 21:07:24 +0100

    update classifiers; bump version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='stratuslab-libcloud-drivers',
-    version='0.4',
+    version='0.2',
     author='StratusLab',
     author_email='contact@stratuslab.eu',
     url='http://stratuslab.eu/',
@@ -13,7 +13,21 @@ setup(
     packages=['stratuslab', 'stratuslab.libcloud'],
 
     install_requires=[
-        "apache-libcloud >= 0.11.4",
-        "stratuslab-client == 13.02.0",
-    ],
+        "apache-libcloud",
+        "stratuslab-client",
+        ],
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Natural Language :: English',
+        'Operating System :: POSIX',
+        'Operating System :: Unix',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Programming Language :: Python :: 2.6',
+        'Topic :: System :: Distributed Computing',
+        ],
+
 )
