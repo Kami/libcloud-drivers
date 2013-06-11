65b6679113a0533b1e805554a8fd7c5a2d0a51e7
Charles Loomis <cal@sixsq.com>
2013-06-11 10:23:48 +0200

    use standard build layout; update to latest libcloud

--- src/main/python/setup.py
+++ src/main/python/setup.py
@@ -1,19 +1,19 @@
 from distutils.core import setup
 
 setup(
-    name='stratuslab-libcloud-drivers',
-    version='0.9',
+    name='${project.artifactId}',
+    version='${project.version}',
     author='StratusLab',
     author_email='contact@stratuslab.eu',
     url='http://stratuslab.eu/',
     license='Apache 2.0',
-    description='Libcloud drivers for StratusLab clouds',
+    description='${project.description}',
     long_description=open('README.txt').read(),
 
     packages=['stratuslab', 'stratuslab.libcloud'],
 
     install_requires=[
-        "apache-libcloud ==0.12.1",
+        "apache-libcloud ==0.12.4",
         "stratuslab-client",
         ],
 
--- src/main/resources/README.txt
+++ src/main/resources/README.txt
@@ -14,15 +14,9 @@ at the collaboration's website: http://stratuslab.eu/.  Information
 about Libcloud is available from the Apache site:
 http://libcloud.apache.org/.
 
-Warning
-=======
-
-*This is beta code.* There may be significant bugs in the
-implementation and the code itself may change in incompatible ways
-without notice.  Use at your own risk!
-
 Feedback
 ========
 
 To provide feedback about this driver or to report bugs, please send
-an email to support@stratuslab.eu.
+an email to support@stratuslab.eu or open an issue on
+[GitHub](https://github.com/StratusLab/libcloud-drivers/issues).
